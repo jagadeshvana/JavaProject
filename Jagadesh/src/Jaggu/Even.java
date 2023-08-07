9d9418b3fd6eda3da9a77a0fe27ce4f5ddfdfdd4
jagad <you@example.com>
2023-08-07 15:22:56 +0530

    First Commit

---
+++ Jagadesh/src/Jaggu/Even.java
@@ -0,0 +1,23 @@
+package Jaggu;
+
+import java.util.Scanner;
+
+public class Even {
+
+	public static void main(String[] args) {
+		int a;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		a=sc.nextInt();
+		if(a%2==0)
+		{
+			System.out.println(a+" is even number");
+		}
+		else
+		{
+			System.out.println(a+" is not even number");
+		}
+
+	}
+
+}
---
+++ Jagadesh/src/Jaggu/Jaga.java
@@ -0,0 +1,5 @@
+package Jaggu;
+
+public class Jaga {
+
+}
